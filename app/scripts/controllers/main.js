54cb098e97b2f7c22d59609148798a2b09e71262
Eugene <yeo.eugene.oey@gmail.com>
2014-05-11 03:56:44 +0800

    static data for books

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,8 @@
 
 angular.module('webappApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.books = [
+      {'isbn': '987444432234', 'title': 'Awesome Demo'},
+      {'isbn': '987888765218', 'title': 'Rock the Party'},
     ];
   });
