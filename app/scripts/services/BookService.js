7df0597ab69e672f60507cf0b88777a30dce98b5
Eugene <yeo.eugene.oey@gmail.com>
2014-05-14 02:39:59 +0800

    sync with api

--- app/scripts/services/BookService.js
+++ app/scripts/services/BookService.js
@@ -2,7 +2,7 @@
 
 angular.module('bswebApp')
   .service('Book', ['$resource', function ($resource) {
-    return $resource('http://localhost:8000/api/v1/:bookGenericId', {}, {
-        query: {method: 'GET', params: {bookGenericId: 'books-generic'}, isArray: true}
+    return $resource('http://localhost:8000/api/v1/:bookProfileId', {}, {
+        query: {method: 'GET', params: {bookProfileId: 'books-profile'}, isArray: true}
       });
   }]);
