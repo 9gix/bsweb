90c52b482791ae91b2a1222d8d6d35426892bdc3
Eugene <yeo.eugene.oey@gmail.com>
2014-05-19 01:25:39 +0800

    Fix API URL

--- app/scripts/services/BookService.js
+++ app/scripts/services/BookService.js
@@ -2,7 +2,7 @@
 
 angular.module('bswebApp')
   .service('Book', ['$resource', function ($resource) {
-    return $resource('http://localhost:8000/api/v1/:bookProfileId', {}, {
+    return $resource('http://localhost:8000/:bookProfileId', {}, {
         query: {method: 'GET', params: {bookProfileId: 'books-profile'}, isArray: true}
       });
   }]);
