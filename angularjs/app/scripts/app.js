3c1cc8afc3604c6aaefdcb37de5f74dc54146a69
Eugene <yeo.eugene.oey@gmail.com>
2014-06-13 23:44:00 +0800

    Ui Router basic setup

--- angularjs/app/scripts/app.js
+++ angularjs/app/scripts/app.js
@@ -1,24 +1,60 @@
 'use strict';
 
 angular.module('bswebApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'ui.bootstrap',
-  'ui.router',
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'ui.bootstrap',
+    'ui.router',
+  ])
+  .config(['$stateProvider', 'ACCESS_LEVELS', '$urlRouterProvider',
+          function ($stateProvider, ACCESS_LEVELS, $urlRouterProvider) {
+
+      $urlRouterProvider.when('', '/index.html');
+      // Anonymous Routes
+      $stateProvider
+        .state('anon', {
+          abstract: true,
+          template: '<ui-view/>',
+          data: {
+              access: ACCESS_LEVELS.anon,
+          }
       })
-      .when('/search', {
-        templateUrl: 'views/search.html',
-        controller: 'SearchCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
+      .state('anon.login', {
+          template: ''
       });
-  });
+    $stateProvider
+      .state('anon.404', {
+        url: '/404/',
+        templateUrl: '404',
+      });
+
+    $stateProvider
+      .state('home', {
+          url: '/index.html',
+          template: 'hello',
+      });
+
+    // User Routes
+    $stateProvider
+      .state('user', {
+          abstract: true,
+          template: '<ui-view/>',
+          url: '/manage',
+          data: {
+              access: ACCESS_LEVELS.user,
+          }
+      });
+
+    // Admin Routes
+    $stateProvider
+      .state('admin', {
+          abstract: true,
+          url: '/admin',
+          template: '<ui-view/>',
+          data: {
+              access: ACCESS_LEVELS.user,
+          }
+      });
+  }]);
