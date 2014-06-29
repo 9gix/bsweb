db1f66d9f91d9c760f7f9da72c6a056ee9e205e4
Eugene <yeo.eugene.oey@gmail.com>
2014-06-29 19:49:02 +0800

    add book modal search result skeleton

--- app/scripts/controllers/mybookadd.js
+++ app/scripts/controllers/mybookadd.js
@@ -10,7 +10,15 @@
 angular.module('bswebApp')
   .controller('MyBookAddCtrl', function ($scope, $modalInstance) {
 
-    $scope.book = {};
+    $scope.books = {};
+    $scope.searchResult = {
+      books: [
+        {isbn13: '123', selected: 1},
+        {isbn13: '321', selected: 0},
+        {isbn13: '444', selected: -1},
+      ],
+
+    };
 
     $scope.add = function(){
       $modalInstance.close($scope.book);
